f27f8097ffdbc1582609bfac1b30167a588cf174
AllaVinner <joel.w.ottosson@gmail.com>
2022-05-14 13:43:49 +0200

    Minor changes

--- R/filterPanel.R
+++ R/filterPanel.R
@@ -1,7 +1,13 @@
 library(tidyverse)
 library(shiny)
 
-ll <- starwars %>% discard(is.list)
+ll <- starwars %>% discard(~!is.list(.x)) %>% names()
+
+numericalFilterComponent <- function(name, label, atomic) {
+  min_val <- min(atomic)
+  max_val <- max(atomic)
+  sliderInput(name, label, min = min_val, max = max_val, value = c(min_val, max_val), ticks = FALSE)
+}
 
 
 filterApp <- function() {
@@ -10,7 +16,7 @@ filterApp <- function() {
     sidebarLayout(
       sidebarPanel(
            sliderInput('int_slider', 'my slider', min = -4, max = 10, value = 3, step = 1, ticks = FALSE),
-           combo
+           uiOutput('filter'),
       ),
       mainPanel(
         tableOutput('table')
@@ -21,6 +27,14 @@ filterApp <- function() {
     output$table <- renderTable({
       df
     })
+    filterables <- reactive({
+      f <- df %>% discard(~!is.atomic(.x)) %>% names()
+      f
+    })
+    output$filter <- renderUI({
+      numericalFilterComponent('mpg', 'MPG', df$mpg )
+    })
+    
   }
   shinyApp(ui, server)
 }
